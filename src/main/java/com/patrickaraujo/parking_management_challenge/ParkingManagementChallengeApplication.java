331c9d1d9e87a3113068d7011ae7760f01e86ddf
PatrickAraujoD <lordpatrick200@gmail.com>
2025-03-29 09:30:24 -0300

    first commit

---
+++ src/main/java/com/patrickaraujo/parking_management_challenge/ParkingManagementChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.patrickaraujo.parking_management_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrickaraujo/parking_management_challenge/ParkingManagementChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrickaraujo.parking_management_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
