1e46633fdb9899005ec9acf88cfbb62e06c43bea
PatrickAraujoD <lordpatrick200@gmail.com>
2025-03-29 18:55:24 -0300

    feat: add new method findById

--- src/main/java/com/patrickaraujo/parking_management_challenge/adapters/EstablishmentRepository.java
+++ src/main/java/com/patrickaraujo/parking_management_challenge/adapters/EstablishmentRepository.java
@@ -1,8 +1,11 @@
 package com.patrickaraujo.parking_management_challenge.adapters;
 
+import java.util.Optional;
+
 import com.patrickaraujo.parking_management_challenge.core.models.Establishment;
 
 public interface EstablishmentRepository {
   Establishment findEstablishmentByCnpj(String cnpj);
   Establishment save(Establishment establishment);
+  Optional<Establishment> findById(String id);
 }
