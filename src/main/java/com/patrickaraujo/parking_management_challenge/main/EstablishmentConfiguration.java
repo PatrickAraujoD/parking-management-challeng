0203742b608dd33f764ebc13354f35f3852fec94
PatrickAraujoD <lordpatrick200@gmail.com>
2025-03-29 11:27:50 -0300

    feat: add endpoint to create new establishment

---
+++ src/main/java/com/patrickaraujo/parking_management_challenge/main/EstablishmentConfiguration.java
@@ -0,0 +1,29 @@
+package com.patrickaraujo.parking_management_challenge.main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.patrickaraujo.parking_management_challenge.adapters.EstablishmentRepository;
+import com.patrickaraujo.parking_management_challenge.application.establishment.AddEstablishmentService;
+import com.patrickaraujo.parking_management_challenge.core.usecases.establishment.AddEstablishment;
+import com.patrickaraujo.parking_management_challenge.infra.gateways.establishment.EstablishmentMapper;
+import com.patrickaraujo.parking_management_challenge.infra.gateways.establishment.EstablishmentRepositoryGateway;
+import com.patrickaraujo.parking_management_challenge.infra.persistence.establishment.EstablishmentRepositoryJpa;
+
+@Configuration
+public class EstablishmentConfiguration {
+  @Bean
+  public AddEstablishment addEstablishment(EstablishmentRepository establishmentRepository) {
+    return new AddEstablishmentService(establishmentRepository);
+  }
+
+  @Bean
+  public EstablishmentRepository establishmentRepository(EstablishmentRepositoryJpa establishmentRepositoryJpa, EstablishmentMapper establishmentMapper) {
+    return new EstablishmentRepositoryGateway(establishmentRepositoryJpa, establishmentMapper);
+  }
+
+  @Bean
+  public EstablishmentMapper establishmentMapper() {
+    return new EstablishmentMapper();
+  }
+}
