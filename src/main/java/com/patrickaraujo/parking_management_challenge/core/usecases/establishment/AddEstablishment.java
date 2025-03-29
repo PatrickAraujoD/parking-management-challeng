2a23fbe2c1dfd5b94e6160c6f9f91eb77a1ba4f2
PatrickAraujoD <lordpatrick200@gmail.com>
2025-03-29 09:46:52 -0300

    feat: add AddEstablishment usecase interface

---
+++ src/main/java/com/patrickaraujo/parking_management_challenge/core/usecases/establishment/AddEstablishment.java
@@ -0,0 +1,7 @@
+package com.patrickaraujo.parking_management_challenge.core.usecases.establishment;
+
+import com.patrickaraujo.parking_management_challenge.core.models.Establishment;
+
+public interface AddEstablishment {
+  Establishment add (Establishment establishment);
+}
