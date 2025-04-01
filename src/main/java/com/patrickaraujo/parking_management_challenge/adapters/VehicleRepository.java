4317ff8144be432990f132acea50ecf0efe34cec
PatrickAraujoD <lordpatrick200@gmail.com>
2025-03-31 21:48:45 -0300

    feat: add VehicleRepository interface

---
+++ src/main/java/com/patrickaraujo/parking_management_challenge/adapters/VehicleRepository.java
@@ -0,0 +1,11 @@
+package com.patrickaraujo.parking_management_challenge.adapters;
+
+import java.util.Optional;
+
+import com.patrickaraujo.parking_management_challenge.core.models.Vehicle;
+
+public interface VehicleRepository {
+  Optional<Vehicle> findById(String id);
+  Optional<Vehicle> findByPlate(String plate);
+  Vehicle save(Vehicle vehicle);
+}
