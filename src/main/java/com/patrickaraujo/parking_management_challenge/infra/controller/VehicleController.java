277ec94798309d7c88f55548b1f4c115813918c5
PatrickAraujoD <lordpatrick200@gmail.com>
2025-04-01 18:33:22 -0300

    feat: add route to update vehicle color

--- src/main/java/com/patrickaraujo/parking_management_challenge/infra/controller/VehicleController.java
+++ src/main/java/com/patrickaraujo/parking_management_challenge/infra/controller/VehicleController.java
@@ -2,27 +2,38 @@ package com.patrickaraujo.parking_management_challenge.infra.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patrickaraujo.parking_management_challenge.core.models.Vehicle;
 import com.patrickaraujo.parking_management_challenge.core.usecases.vehicles.AddVehicle;
+import com.patrickaraujo.parking_management_challenge.core.usecases.vehicles.UpdateVehicle;
+import com.patrickaraujo.parking_management_challenge.infra.dtos.vehicle.UpdateVehicleDTO;
 import com.patrickaraujo.parking_management_challenge.infra.dtos.vehicle.VehicleDTO;
 
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
   private AddVehicle addVehicle;
+  private UpdateVehicle updateVehicle;
 
-  public VehicleController(AddVehicle addVehicle) {
+  public VehicleController(AddVehicle addVehicle, UpdateVehicle updateVehicle) {
     this.addVehicle = addVehicle;
+    this.updateVehicle = updateVehicle;
   }
 
   @PostMapping
   public ResponseEntity<Void> createVehicle(@RequestBody VehicleDTO data) {
     Vehicle newVehicle = new Vehicle(data.mark(), data.model(), data.color(), data.plate(), data.vehicleType());
     this.addVehicle.add(newVehicle);
+    return ResponseEntity.ok().build();
+  }
+
+  @PutMapping
+  public ResponseEntity<Void> updateVehicle(@RequestBody UpdateVehicleDTO data) {
+    this.updateVehicle.update(data.id(), data.color());
     return ResponseEntity.ok().build();
   }
 }
--- src/main/java/com/patrickaraujo/parking_management_challenge/main/VehicleConfiguration.java
+++ src/main/java/com/patrickaraujo/parking_management_challenge/main/VehicleConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 import com.patrickaraujo.parking_management_challenge.adapters.VehicleRepository;
 import com.patrickaraujo.parking_management_challenge.application.vehicle.AddVehicleService;
+import com.patrickaraujo.parking_management_challenge.application.vehicle.UpdateVehicleService;
 import com.patrickaraujo.parking_management_challenge.core.usecases.vehicles.AddVehicle;
+import com.patrickaraujo.parking_management_challenge.core.usecases.vehicles.UpdateVehicle;
 import com.patrickaraujo.parking_management_challenge.infra.gateways.vehicle.VehicleMapper;
 import com.patrickaraujo.parking_management_challenge.infra.gateways.vehicle.VehicleRepositoryGateway;
 import com.patrickaraujo.parking_management_challenge.infra.persistence.vehicle.VehicleRepositoryJpa;
@@ -15,6 +17,11 @@ public class VehicleConfiguration {
   @Bean
   public AddVehicle addVehicle(VehicleRepository vehicleRepository) {
     return new AddVehicleService(vehicleRepository);
+  }
+
+  @Bean
+  public UpdateVehicle updateVehicle(VehicleRepository vehicleRepository) {
+    return new UpdateVehicleService(vehicleRepository);
   }
 
   @Bean
