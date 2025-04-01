9189fa4c1297ab7b5632de56532d391cce1777ae
PatrickAraujoD <lordpatrick200@gmail.com>
2025-03-31 21:55:07 -0300

    feat: add mapped entity to database

---
+++ src/main/java/com/patrickaraujo/parking_management_challenge/infra/persistence/vehicle/VehicleEntity.java
@@ -0,0 +1,32 @@
+package com.patrickaraujo.parking_management_challenge.infra.persistence.vehicle;
+
+import com.patrickaraujo.parking_management_challenge.core.models.VehicleType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "vehicles")
+@Table(name = "vehicles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class VehicleEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+  private String mark;
+  private String model;
+  private String color;
+  private String plate;
+  private VehicleType typeVehicle;
+}
