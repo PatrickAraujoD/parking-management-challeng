3ddf31b7c16d2261ae56c696349353bb9fee340b
PatrickAraujoD <lordpatrick200@gmail.com>
2025-03-31 23:12:27 -0300

    feat: add VehiclesDTO

---
+++ src/main/java/com/patrickaraujo/parking_management_challenge/infra/dtos/vehicle/VehicleDTO.java
@@ -0,0 +1,13 @@
+package com.patrickaraujo.parking_management_challenge.infra.dtos.vehicle;
+
+import com.patrickaraujo.parking_management_challenge.core.models.VehicleType;
+
+public record VehicleDTO(
+  String id,
+  String mark,
+  String plate,
+  String model,
+  String color,
+  VehicleType vehicleType
+) {
+}
