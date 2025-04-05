4d8976bc7614d0e947cc4b13f6d4504e38e2813a
PatrickAraujoD <lordpatrick200@gmail.com>
2025-04-05 10:40:34 -0300

    feat: add VehicleExitControl usecase

---
+++ src/main/java/com/patrickaraujo/parking_management_challenge/core/usecases/establishment/VehicleExitControl.java
@@ -0,0 +1,5 @@
+package com.patrickaraujo.parking_management_challenge.core.usecases.establishment;
+
+public interface VehicleExitControl {
+  String control (String idEstablishment, String idVehicle);
+}
